2e7258f0435fc966b0db23fe6d1efc1ca47c2569
colesa13 <sweetcake61091@yahoo.com>
2024-08-28 14:17:26 -0400

    added styling to inspiration and tips page. compressed pics.

--- src/views/Inspiration/Inspiration.js
+++ src/views/Inspiration/Inspiration.js
@@ -1,5 +1,7 @@
 import Header from '../../components/Header/Header.js';
 import Footer from '../../components/Footer/Footer.js';
+import InspirationImage from '../../assets/images/inspiration_main-image.jpg';
+import './Inspiration.css';
 
 
 
@@ -11,6 +13,18 @@ function InspirationPage() {
         <Header />
         <div className='main'>
             <h2>Inspiration</h2>
+            <img src={InspirationImage} alt='inspiration pic' className='inspiration-main-image'/>
+
+            <div className='inspiration-main-content'>
+                <div className='inspiration-content'>
+                    <h3>Title</h3>
+                    <p>Paragraph stating title of content</p>
+                </div>
+                <div className='inspiration-content'>
+                    <h3>About the Artist</h3>
+                    <p>Paragraph describing information about the artist of the content</p>
+                </div>
+            </div>
            
         </div>
         <Footer />
--- src/views/TipsPage/TipsPage.js
+++ src/views/TipsPage/TipsPage.js
@@ -1,7 +1,20 @@
 import Header from '../../components/Header/Header.js';
 import Footer from '../../components/Footer/Footer.js';
+import TipLogo from '../../assets/logo/tip_logo.png';
+import './TipsPage.css';
 
 
+function TipCard () {
+
+    
+    return (
+        <div className='tip-card'>
+            <img src={TipLogo} alt='Tip Logo' className='tip-logo' />
+            <h3>Easyl Tip #</h3>
+            <p>Content Here</p>
+        </div>
+    )
+}
 
 
 function TipsPage() {
@@ -11,7 +24,12 @@ function TipsPage() {
         <Header />
         <div className='main'>
             <h2>Tips</h2>
-           
+            <div className='tips-outer-div'>
+                <TipCard />
+                <TipCard />
+                <TipCard />
+                <TipCard />
+            </div>
         </div>
         <Footer />
       </>
