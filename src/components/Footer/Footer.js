0c5ffbf2a87efdb8928ebf2bb662fa0bfde1f6ff
colesa13 <sweetcake61091@yahoo.com>
2024-09-04 11:45:40 -0400

    updated footer by adding links to artforms

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import FooterLogo from '../../assets/logo/other_logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faPinterest, faInstagram } from '@fortawesome/free-brands-svg-icons';
 //import { fa } from '@fortawesome/free-solid-svg-icons';
 //import { faRegular } from '@fortawesome/free-regular-svg.icons';
+import Artforms from "../../views/ArtformPages/ArtformsData";
 import './Footer.css';
 
 function Footer() {
+
+    function FooterLink() {
+        return (
+
+              <ul>
+                {Artforms.map((nav) => 
+                    <li key={nav.id}>
+                        <Link to={`/${nav.path}`}>{nav.name}</Link>
+                    </li>
+                )}
+            </ul>
+     
+        );
+    }
+
     return (
         <footer>
-            <img src={FooterLogo} alt='logo' className='footerLogo'/>
-            <div className='extraLinks'>
-                <p>Other links to pages will be here</p>
+            <Link to='/'><img src={FooterLogo} alt='logo' className='footerLogo'/></Link>
+            <div className='footer-links'>
+                <FooterLink />
             </div>
             <div className='footerIcons'>
                 <p><FontAwesomeIcon icon={faFacebook} size ='3x' className='icon-color-change' /></p>
